58dc2ea292a6547dcd8a6c0c4aca723a74c6aaac
miriamriouchmhine <miriamriouchmhine@gmail.com>
2023-04-03 20:18:40 +0200

    Tests, cambio de direccion, config.py

--- mylib/config.py
+++ mylib/config.py
@@ -5,7 +5,7 @@
 # e.g., 'email@gmail.com'
 MAIL = ''
 # Enter the ip camera url (e.g., url = 'http://191.138.0.100:8040/video')
-url = "rtsp://tapo2912:Riouch2000@192.168.102.120:554/h264/ch1/main/av_stream"
+url = "rtsp://biblioteca:camaraBibAlex@192.168.102.120:554/h264/ch1/main/av_stream"
 
 # ON/OFF for mail feature. Enter True to turn on the email alert feature.
 ALERT = False
